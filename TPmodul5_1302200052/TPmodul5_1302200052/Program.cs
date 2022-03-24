942301dd8e680b19918d141032faa435ada76455
glenrioaristo <glenrioaristo@student.telkomuniversity.ac.id>
2022-03-24 22:47:30 +0700

    menambahkan class SayaTubeVideo

---
+++ TPmodul5_1302200052/TPmodul5_1302200052/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TPmodul5_1302200052
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – glen rio aristo");
+            video.IncreasePlayCount(15);
+            video.PrintVideoDetails();
+        }
+    }
+}
---
+++ TPmodul5_1302200052/TPmodul5_1302200052/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul5_1302200052
+{
+    internal class SayaTubeVideo
+    {
+        private int id, playCount;
+        private string title;
+
+        public SayaTubeVideo (string title)
+        {
+
+            this.title = title;
+            playCount = 0;
+
+            Random rndm = new Random();
+            id = rndm.Next(10000,99999);//returns random integers < 10
+            
+          
+        }
+
+        public void IncreasePlayCount(int x)
+        {
+            playCount = x;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("ID :"+id +", Judul :"+ title +", Jumlah play :"+ playCount);
+        }
+    }
+}
